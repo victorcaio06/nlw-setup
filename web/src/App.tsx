3adc1c12b6341a35a7f73ca223517c9bdb4aa7dc
Victor Caio <kaioaraujo426@gmail.com>
2023-01-21 23:00:53 -0300

    FEAT: created the header for the site

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,16 @@
-import './styles/global.css'
+import './styles/global.css';
 
-import { Habit } from "./components/Habit";
+import { Plus } from 'phosphor-react';
+import logoImage from './assets/Logo.svg';
+import { Header } from './components/Header';
 
-function App() {
+export function App() {
 
   return (
-    <div>
-        <Habit completed={2}/>
-        <Habit completed={3}/>
-        <Habit completed={8}/>
-        <Habit completed={6}/>
-        <Habit completed={10}/>
-        <Habit completed={23}/>
-        <Habit completed={11}/>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
