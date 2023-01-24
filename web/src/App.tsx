6eb09da4d6beee01d992af7fb2fed0e7e5a89cac
Victor Caio <kaioaraujo426@gmail.com>
2023-01-24 05:35:26 -0300

    FEAT: incomplete homepage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,7 @@
 import './styles/global.css';
 
-import { Plus } from 'phosphor-react';
-import logoImage from './assets/Logo.svg';
 import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
 export function App() {
 
@@ -10,6 +9,7 @@ export function App() {
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
         <Header />
+        <SummaryTable />
       </div>
     </div>
   );
