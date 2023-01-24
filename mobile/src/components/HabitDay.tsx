85140c2664ec212907cbe3447cee15214563be07
Victor Caio <kaioaraujo426@gmail.com>
2023-01-24 18:54:23 -0300

    FEAT: created first habit square and added days of the week

---
+++ mobile/src/components/HabitDay.tsx
@@ -0,0 +1,24 @@
+import { Dimensions, TouchableOpacity } from "react-native";
+
+const WeekDays = 7;
+
+const ScreenHorizontalPadding = (32 * 2) / 5;
+
+export const MarginBetween = 8;
+
+export const DaySize = (Dimensions.get('screen').width / WeekDays) - (ScreenHorizontalPadding + 5);
+
+export function HabitDay() {
+  return (
+    <TouchableOpacity 
+      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      style={
+        {
+          width: DaySize, height: DaySize
+        }
+      }
+    />
+
+      
+  );
+}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,32 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+
+const WeekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+import { HabitDay, DaySize, MarginBetween } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
+      <View
+        className="flex-row mt-6 mb-2"
+      >
+        {
+          WeekDays.map((day, index) => (
+            <Text 
+              key={index}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{width: DaySize}}
+            >
+              {day}
+            </Text>
+          ))
+        }
+      </View>
+
+      <HabitDay/>
     </View>
   );
 }
