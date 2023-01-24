3f8b9e0cb29ff0b40dcde75221d9098935251235
Victor Caio <kaioaraujo426@gmail.com>
2023-01-24 06:32:10 -0300

    FEAT: configured nativewind

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090a',
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      }
+    },
   },
   plugins: [],
 }
