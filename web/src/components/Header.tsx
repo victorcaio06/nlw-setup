cc5f54f32817b6a7fb36da8e20c54f6b0936ba13
Victor Caio <kaioaraujo426@gmail.com>
2023-01-25 16:33:53 -0300

    FEAT: finished header of modal of create a new habit

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,5 @@
-import { Plus, X } from "phosphor-react";
-import { useState } from "react";
 import * as Dialog from '@radix-ui/react-dialog';
+import { Plus, X } from "phosphor-react";
 
 import logoImage from '../assets/Logo.svg';
 
@@ -19,13 +18,23 @@ export function Header() {
         </Dialog.Trigger>
 
         <Dialog.Portal>
-          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
-          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full 
+          <Dialog.Overlay 
+            className="w-screen h-screen bg-black/80 fixed inset-0" 
+          />
+          <Dialog.Content 
+            className="absolute p-10 bg-zinc-900 rounded-2xl w-full 
             max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" 
           >
-            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
+            <Dialog.Close 
+              className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200"
+            >
               <X size={24} aria-label="Fechar" />
             </Dialog.Close>
+            <Dialog.Title 
+              className="text-3xl leading-tight text-white font-extrabold" 
+            >
+              Criar Hábito
+            </Dialog.Title>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
