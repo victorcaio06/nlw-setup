3a89ffe1b308ba6c1f70be96808ebeef2c5d41d7
Victor Caio <kaioaraujo426@gmail.com>
2023-01-26 00:47:46 -0300

    FEAT: routes created

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,6 +10,7 @@ import { StatusBar } from 'react-native';
 
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
 
@@ -26,7 +27,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
     </>
   );
