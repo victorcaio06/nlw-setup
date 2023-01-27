eaf91144428aa28f7446d78d2e9ef180a19b2ffd
Victor Caio <kaioaraujo426@gmail.com>
2023-01-27 15:57:23 -0300

    FEAT: marking habits how completed

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -19,6 +19,7 @@ interface HabitsInfo {
 
 export function HabitsList({ date }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+
   useEffect(() => {
     API.get("/day", {
       params: {
@@ -31,12 +32,38 @@ export function HabitsList({ date }: HabitsListProps) {
 
   const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
 
+  async function handleToggleHabit(habitId: string) {
+    await API.patch(`/habits/${habitId}/toggle`);
+
+    const isHabitAlreadyCompleted =
+      habitsInfo!.completedHabits.includes(habitId);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(
+        (id) => id !== habitId
+      );
+
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+    
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    });
+  }
+
   return (
     <div className="mt-6 flex flex-col gap3">
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
             key={habit.id}
+            onCheckedChange={() => {
+              handleToggleHabit(habit.id);
+            }}
             checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}
             className="flex items-center gap-3 group"
