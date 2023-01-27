7177474baac61e177e4b8b139d8485a4d6e3160c
Victor Caio <kaioaraujo426@gmail.com>
2023-01-27 11:58:39 -0300

    FEAT: changed some confirm button details

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -90,7 +90,7 @@ export function NewHabitForm() {
       <button
         type="submit"
         className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center 
-        font-semibold bg-green-600 hover:bg-green-500 text-black hover:text-white"
+        font-semibold bg-violet-500 hover:bg-violet-700 text-black hover:text-white"
       >
         <Check size={20} weight="bold" />
         Confirmar
