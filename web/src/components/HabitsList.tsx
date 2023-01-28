56d786e25310f75b71c900e27c03be2c7398901e
Victor Caio <kaioaraujo426@gmail.com>
2023-01-28 01:56:28 -0300

    FEAT: added details for focus

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -54,11 +54,11 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
       completedHabits,
     });
 
-    onCompletedChanged(completedHabits.length)
+    onCompletedChanged(completedHabits.length);
   }
 
   return (
-    <div className="mt-6 flex flex-col gap3">
+    <div className="mt-6 flex flex-col gap-3 ">
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
@@ -68,11 +68,11 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
             }}
             checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
           >
             <div
               className=" h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800
-          group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors duration-500"
+          group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors duration-500 group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background"
             >
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -51,7 +51,7 @@ export function NewHabitForm() {
         type="text"
         id="title"
         placeholder="Ex: Exercícios, estudar, comer..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         value={title}
         onChange={(event) => setTitle(event.target.value)}
@@ -66,7 +66,7 @@ export function NewHabitForm() {
           return (
             <Checkbox.Root
               key={`${weekDay}-${index}`}
-              className="flex items-center gap-3 group"
+              className="flex items-center gap-3 group focus:outline-none"
               checked={weekDays.includes(index)}
               onCheckedChange={() => {
                 handleToggleWeekDay(index);
@@ -74,10 +74,10 @@ export function NewHabitForm() {
             >
               <div
                 className=" h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800
-              group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors duration-500"
+              group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors duration-500 group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900"
               >
                 <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
+                  <Check size={20} className="text-white " />
                 </Checkbox.Indicator>
               </div>
 
@@ -90,7 +90,7 @@ export function NewHabitForm() {
       <button
         type="submit"
         className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center 
-        font-semibold bg-violet-500 hover:bg-violet-700 text-black hover:text-white transition-colors duration-300"
+        font-semibold bg-violet-500 hover:bg-violet-700 text-black hover:text-white transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:text-white"
       >
         <Check size={20} weight="bold" />
         Confirmar
