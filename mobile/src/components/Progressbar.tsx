978c3be593aeb1cc4f2fa918b88b2e3158f399e5
Victor Caio <kaioaraujo426@gmail.com>
2023-01-29 04:35:53 -0300

    FEAT: added animation in progress bar

--- mobile/src/components/Progressbar.tsx
+++ mobile/src/components/Progressbar.tsx
@@ -1,18 +1,34 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 interface Props {
   progress?: number;
 }
 
 export function Progressbar({ progress = 0 }: Props) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress, { duration: 500 });
+  }, [progress]);
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
+      <Animated.View
         className="h-3 rounded-xl bg-violet-600"
-        style={{
-          width: `${progress}%`,
-        }}
-      ></View>
+        style={style}
+      ></Animated.View>
     </View>
   );
 }
